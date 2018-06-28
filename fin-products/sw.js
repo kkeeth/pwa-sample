09c4148cd4076e4714d7cf59551a965b0c7d21ad
k-kuwahara <zensin0082@gmail.com>
2018-06-28 19:37:27 +0900

    fixed: sw.js function

--- fin-products/sw.js
+++ fin-products/sw.js
@@ -1,3 +1,15 @@
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/',
+  '/public/sw.js'
+];
 self.addEventListener('fetch', function(event) {
-  // any funcion
+  // インストール処理
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
