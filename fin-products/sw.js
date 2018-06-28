caef822ee3afea864788c570919c59375d0c8249
yumemi-kuwahara <k_kuwahara@yumemi.co.jp>
2018-06-28 18:56:51 +0900

    fixed: sw.js about install

--- fin-products/sw.js
+++ fin-products/sw.js
@@ -1,3 +1,16 @@
-self.addEventListener('fetch', function(event) {
-  // any funcion
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/',
+  '/bundle.js'
+];
+
+self.addEventListener('fetch', (event) => {
+  // install
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
