35e05c3cc295574376e8eccf340910a61064864c
k-kuwahara <zensin0082@gmail.com>
2018-07-02 00:16:21 +0900

    fixed: don't work service worker

--- fin-products/sw.js
+++ fin-products/sw.js
@@ -1,17 +1,40 @@
 var CACHE_NAME = 'my-site-cache-v1';
 var urlsToCache = [
   '/',
-  './index.html',
-  '/public/sw.js'
+  '/index.html',
+  '/bundle.js'
 ];
 
-self.addEventListener('fetch', (event) => {
-  // インストール処理
-  event.waitUntil(
+self.addEventListener('install', (e) => {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
     caches.open(CACHE_NAME)
       .then((cache) => {
-        console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
-  );
-});
+  )
+})
+
+self.addEventListener('fetch', (e) => {
+  console.log('[ServiceWorker] Fetch');
+  e.respondWith(
+    caches.match(e.request)
+      .then((response) => {
+        return response || fetch(e.request);
+      })
+  )
+})
+
+self.addEventListener('activate', (e) => {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== cacheName && key !== dataCacheName) {
+          console.log('[ServiceWorker] Removing old cache', key)
+          return caches.delete(key)
+        }
+      }))
+    })
+  )
+})
