15dbc8484d4376ba6c88ebfc6266551fcbeb1343
k-kuwahara <zensin0082@gmail.com>
2020-08-02 15:41:19 +0900

    feat: added images for manifest

---
+++ public/sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'my-site-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/bundle.js',
+  '/img/'
+];
+
+self.addEventListener('install', (e) => {
+    console.log('[ServiceWorker] Install');
+    e.waitUntil(
+      caches.open(CACHE_NAME)
+        .then((cache) => {
+          return cache.addAll(urlsToCache);
+        })
+    )
+  })
+
+self.addEventListener('fetch', (e) => {
+    console.log('[ServiceWorker] Fetch');
+    e.respondWith(
+      caches.match(e.request)
+        .then((response) => {
+          return response || fetch(e.request);
+        })
+    )
+  })
+  
+  self.addEventListener('activate', (e) => {
+    console.log('[ServiceWorker] Activate');
+    e.waitUntil(
+      caches.keys().then((keyList) => {
+        return Promise.all(keyList.map((key) => {
+          if (key !== CACHE_NAME && key !== dataCacheName) {
+            console.log('[ServiceWorker] Removing old cache', key)
+            return caches.delete(key)
+          }
+        }))
+      })
+    )
+  })
