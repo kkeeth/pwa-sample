0c0d98b80cee4d98a0634ba88a41654bdedb97f6
kkeeth <zensin0082@gmail.com>
2021-06-11 19:59:21 +0900

    chore: rename output directory

--- rollup.config.js
+++ rollup.config.js
@@ -7,7 +7,7 @@ import postcssCssnext from "postcss-cssnext";
 export default {
   input: "src/main.js",
   output: {
-    file: "public/bundle.js",
+    file: "docs/bundle.js",
     format: "iife",
   },
   plugins: [
